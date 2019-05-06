1d72a70d6f11d59d28671dd65d25676acbdf1046
Alan Manning <aamanning@crimson.ua.edu>
2019-05-06 02:40:02 -0500

    Small change to gatsby-config.js - removed index.php for baseUrl field

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
          * The base URL of the Wordpress site without the trailingslash and the protocol. This is required.
          * Example : 'gatsbyjsexamplewordpress.wordpress.com' or 'www.example-site.com'
          */
-        baseUrl: "localhost/PersonalSite.io/index.php/",
+        baseUrl: "localhost/PersonalSite.io/",
         // The protocol. This can be http or https.
         protocol: "http",
         // Indicates whether the site is hosted on wordpress.com.
